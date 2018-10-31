db8e2cd9cbc56ce6d4fc546079104736e89e5aef
adingler <adingler@gmail.com>
2018-10-31 15:19:59 -0700

    Initial commit of skeleton for Lab 4.

---
+++ recur.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+
+//TODO #1.1: add a function prototype for fib
+
+//TODO #2.1: add a function prototype for fact
+
+int main()
+{
+	//TODO: #1.2 call your fib function, print the result
+	
+	//TODO: #2.2 call your fact function, print the result
+
+	return 0;
+}
+
+//TODO #2.3: add your recursive fib function from class
+
+
+//TODO #2.4: add your recursive fact function from class
